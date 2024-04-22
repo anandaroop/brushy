82ee069f5dedef03eaf38b038f5d78508fabda20
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-04-22 16:33:06 -0400

    style: basic chat styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,55 @@
 "use client";
 
+import Image from "next/image";
 import { useChat } from "ai/react";
+import styles from "./page.module.css";
+import { useRef, useEffect } from "react";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
-  return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === "user" ? "User: " : "AI: "}
-          {m.content}
-        </div>
-      ))}
+  const scrollMeRef = useRef<HTMLDivElement>(null);
 
-      <form onSubmit={handleSubmit}>
+  useEffect(() => {
+    if (scrollMeRef.current) {
+      scrollMeRef.current.scrollIntoView();
+    }
+  }, [messages]);
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.messages}>
+        {messages.length === 0 ? (
+          <div className={styles.banner}>
+            <div>
+              <Image src="/brushy.gif" alt="Brushy" width={100} height={100} />
+            </div>
+            <div>Ask me about artists and artworks on Artsy</div>
+          </div>
+        ) : (
+          <>
+            {messages.map((m) => (
+              <div key={m.id} className={styles.message}>
+                <div className={styles.role}>
+                  {m.role === "user" ? "You" : "Artsy"}
+                </div>
+                <div className={styles.content}>{m.content}</div>
+              </div>
+            ))}
+
+            <div ref={scrollMeRef}>
+              {/* invisible element that will be scrolled into view as `messages` gets updated */}
+            </div>
+          </>
+        )}
+      </div>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          className={styles.userInput}
           value={input}
-          placeholder="Say something..."
+          placeholder="Chat with Artsy"
           onChange={handleInputChange}
         />
       </form>
-    </div>
+    </main>
   );
 }
